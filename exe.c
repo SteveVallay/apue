b2a3c7859e59ff7078e28226279a850b405a51f6
zhibinw <zhibinwang.q@gmail.com>
2013-10-17 06:16:17 +0800

    add \n end message

--- exe.c
+++ exe.c
@@ -24,13 +24,15 @@ int main(void)
        if (0 == pid){
            printf("returned in child process pid is %d \n",pid);
            execlp(buf,buf,(char*)0);
-           err_ret("couldn't execute %s",buf);
+           err_ret("couldn't execute %s\n",buf);
            exit(127);
 
        } else if ( pid > 0 ) {
            printf("return  in parent process pid is %d \n",pid);
            if ((pid = waitpid(pid,&status,0)) == -1)
-               err_sys("error waitpid status is %d",status);
+               err_sys("error waitpid status is %d\n",status);
+           else
+               printf("waitpid status is %d\n",status);
        } else {
            err_sys("fork error\n");
        }
