6c721b3e9e77aacaa383ae231661fd6ae2234705
zhibinw <zhibinwang.q@gmail.com>
2013-11-20 11:16:05 +0800

    try to read the hole

--- read.c
+++ read.c
@@ -1,13 +1,13 @@
 #include "apue.h"
 #include <unistd.h>
 
-#define SIZE  100
+#define SIZE  16394
 
 char buf[SIZE];
 int main(void){
  int size;
  size = read(STDIN_FILENO,buf,SIZE);
  printf("size: %d\n",size);
- printf("buf is: %s\n",buf);
+ printf("buf is: %s\n",buf + 16384);
  exit(0);
 }
