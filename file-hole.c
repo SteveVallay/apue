89eb65a78a6cfe426f33e90469551ae348c99858
zhibinw <zhibinwang.q@gmail.com>
2013-11-20 11:00:36 +0800

    if args > 1 , create file.nohole

--- file-hole.c
+++ file-hole.c
@@ -4,14 +4,27 @@
 char buf1[] = "abcdefghij";
 char buf2[] = "ABCDEFGHIJ";
 
-int main(void) {
+#define TRUE 1;
+#define FALSE 0;
+int hole = TRUE;
+
+int main(int argc , char* argv[]) {
     int fd;
-    if ((fd = creat("file.hole",FILE_MODE)) < 0)
+    if(argc > 1){
+        hole = FALSE;
+    }
+    char* name;
+    if (hole){
+        name = "file.hole";
+    } else {
+        name = "file.nohole";
+    }
+    if ((fd = creat(name,FILE_MODE)) < 0)
         err_sys("create error!");
     if (write(fd, buf1, 10) != 10)
         err_sys("buf1 write error");
 
-    if (lseek(fd, 16384, SEEK_SET) == -1)
+    if (hole && lseek(fd, 16384, SEEK_SET) == -1)
         err_sys("lseek error");
 
     if(write(fd, buf2, 10) != 10)
