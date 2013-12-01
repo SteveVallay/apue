6001f037bd5378be83da2fe30cb42a194230e99d
zhibinw <zhibinwang.q@gmail.com>
2013-12-01 23:46:24 +0800

    return if met error when creating files.

--- umask.c
+++ umask.c
@@ -6,11 +6,11 @@
 int main(int argc , char* argv[]) {
     umask(0);
     if (creat("foo",RWRWRW) < 0) {
-        printf("create foo fail!\n");
+        err_sys("create foo fail!\n");
     }
     umask(S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
     if (creat("bar",RWRWRW) < 0) {
-        printf("create bar fail !\n");
+        err_sys("create bar fail !\n");
     }
     exit(0);
 }
