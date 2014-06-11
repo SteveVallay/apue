85b7b08ba27bd9d49f16ec89ea4aad1fa578f806
zhibinw <zhibinwang.q@gmail.com>
2014-06-11 21:12:15 +0800

    update mkstemp close new created file before exit.

--- mkstemp.c
+++ mkstemp.c
@@ -5,9 +5,11 @@ int main(int argc, char* argv[])
         err_quit("usage: mkstemp <pathnameXXXXXX>\n");
 
     char name[MAXLINE];
+    int fd;
     strcpy(name, argv[1]);
-    if (mkstemp(name) == -1)
+    if ((fd = mkstemp(name)) == -1)
         err_sys("mkstemp fail!\n");
     printf("temp file name: %s\n",name);
+    close(fd);
     exit(0);
 }
