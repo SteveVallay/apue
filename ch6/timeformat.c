1ca041f1cc7c96e7de04b5a4eb202d63840850f9
zhibinw <zhibinwang.q@gmail.com>
2014-06-16 00:34:40 +0800

    add localtime example.

---
+++ ch6/timeformat.c
@@ -0,0 +1,22 @@
+#include "apue.h"
+#include <time.h>
+
+int main(int argc, char* argv[])
+{
+    struct tm *ptm;
+    time_t cur;
+    time(&cur);
+    if ((ptm = localtime(&cur))  == NULL)
+        err_quit("mktime fail!\n");
+
+    printf("tm_sec: %d\n", ptm->tm_sec);
+    printf("tm_min: %d\n", ptm->tm_min);
+    printf("tm_hour: %d\n", ptm->tm_hour);
+    printf("tm_mday: %d\n", ptm->tm_mday);
+    printf("tm_mon: %d\n", ptm->tm_mon);
+    printf("tm_year: %d\n", ptm->tm_year);
+    printf("tm_wday: %d\n", ptm->tm_wday);
+    printf("tm_yday: %d\n", ptm->tm_yday);
+    printf("tm_isdst: %d\n", ptm->tm_isdst);
+    return 0;
+}
