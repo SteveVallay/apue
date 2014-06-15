0449c33a69275aed7934ca7d38dfaf464b880902
zhibinw <zhibinwang.q@gmail.com>
2014-06-15 22:52:34 +0800

    add gethostname example.

---
+++ ch6/gethostname.c
@@ -0,0 +1,13 @@
+#include "apue.h"
+#include <unistd.h>
+#define HOST_NAME_MAX 128
+int main (int argc, char* argv[])
+{
+    char hostname[HOST_NAME_MAX];
+    if (gethostname(hostname, HOST_NAME_MAX) < 0 )
+    {
+        err_quit("gethostname fail!\n");
+    }
+    printf("hostname:%s\n", hostname);
+    return 0;
+}
